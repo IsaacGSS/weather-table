d18d6030d8683bf970ff228c8b1745b9d8def06f
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-09 11:37:41 -0300

    🚧
    setup

---
+++ src/components/MeinCliima.tsx
@@ -0,0 +1,29 @@
+export const MeinClima = () => {
+  return (
+    <main>
+      <section>
+        <img src='' alt='' />
+        <div></div>
+      </section>
+      <div>
+        <h2></h2>
+        <span></span>
+        <span></span>
+      </div>
+      <section>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+    </main>
+  )
+}
